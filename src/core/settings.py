5840374b81a2109723b7d3233f0e3ed205812526
AFzOfficial <abol.mortal50@gmail.com>
2023-09-21 20:17:40 +0330

    fix: production configs

--- src/core/settings.py
+++ src/core/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+# CSRF_TRUSTED_ORIGINS = ['domain.com', 'sub.domain.com']
+
 
 # Application definition
 
@@ -118,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = '/var/www/blog/static' if DEBUG == False else BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/blog/media' if DEBUG == False else BASE_DIR / 'media'
 
 
 # Default primary key field type
