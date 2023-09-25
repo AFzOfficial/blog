e78c9c1291be6265d2f65c5b2846792aec1f6fb3
AFzOfficial <abol.mortal50@gmail.com>
2023-09-25 22:50:37 +0330

    fix: xss bug on markdown rendering

--- src/blog/templatetags/blog_extras.py
+++ src/blog/templatetags/blog_extras.py
@@ -2,10 +2,11 @@ import markdown as md
 
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.utils.html import escape
 
 register = template.Library()
 
 @register.filter
 @stringfilter
 def markdown(value):
-    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
+    return md.markdown(escape(value), extensions=['markdown.extensions.fenced_code'])
