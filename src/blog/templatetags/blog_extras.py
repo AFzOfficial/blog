4b62d788f296dd033aa9ff9b4b47eb9890264bf9
AFzOfficial <abol.mortal50@gmail.com>
2023-09-26 13:47:07 +0330

    fix: markdown rendering

--- src/blog/templatetags/blog_extras.py
+++ src/blog/templatetags/blog_extras.py
@@ -1,12 +1,13 @@
 import markdown as md
+import bleach
 
 from django import template
 from django.template.defaultfilters import stringfilter
-from django.utils.html import escape
+# from django.utils.html import escape
 
 register = template.Library()
 
 @register.filter
 @stringfilter
 def markdown(value):
-    return md.markdown(escape(value), extensions=['markdown.extensions.fenced_code'])
+    return md.markdown(bleach.clean(value), extensions=['markdown.extensions.fenced_code'])
